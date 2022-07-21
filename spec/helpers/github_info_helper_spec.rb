f485834702cd45b314e7888556c95a83ba06d65a
drkarolina <k.drahanchuk@gmail.com>
2022-07-21 20:43:56 +0300

    Create github_info controller

---
+++ spec/helpers/github_info_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the GithubInfoHelper. For example:
+#
+# describe GithubInfoHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe GithubInfoHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
