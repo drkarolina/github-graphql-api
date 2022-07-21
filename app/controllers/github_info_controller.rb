5b53187f6fb3c5ed49ee36d21ac6c8c04307577f
drkarolina <k.drahanchuk@gmail.com>
2022-07-21 23:16:03 +0300

    Create query

--- app/controllers/github_info_controller.rb
+++ app/controllers/github_info_controller.rb
@@ -1,7 +1,47 @@
+require "graphql/client"
+require "graphql/client/http"
 class GithubInfoController < ApplicationController
     def index
     end
 
     def show
+        @query_result = Client.query(IndexQuery, variables: {username: params[:gh_login]}).to_h
+        if @query_result["data"]["user"]
+            @github_login = @query_result["data"]["user"]["login"]
+            @github_name = @query_result["data"]["user"]["name"]
+            @github_repos = @query_result["data"]["user"]["repositories"]["nodes"]
+        else
+            flash[:notice] = "Oops! Something went wrong!"
+            redirect_to root_path
+        end
     end
+
+    private
+    GITHUB_ACCESS_TOKEN = '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'
+    URL = 'https://api.github.com/graphql'
+    HTTP = GraphQL::Client::HTTP.new(URL) do
+        def headers(context)
+          { 
+            "Authorization" => "Bearer #{GITHUB_ACCESS_TOKEN}",
+          }
+        end
+    end 
+    
+    Schema = GraphQL::Client.load_schema("db/schema.json")
+    Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+    
+    IndexQuery = Client.parse <<-'GRAPHQL'
+    query($username: String!) {
+        user(login: $username) {
+            login
+            name
+            repositories(first: 50, isFork: false) {
+                nodes {
+                    name
+                    url
+                }
+            }
+        }
+    } 
+    GRAPHQL
 end
