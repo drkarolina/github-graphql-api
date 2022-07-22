6d08c008bd0517477f6f72479e17382ca12d67b0
drkarolina <k.drahanchuk@gmail.com>
2022-07-22 16:54:47 +0300

    Code refactoring

--- app/controllers/github_info_controller.rb
+++ app/controllers/github_info_controller.rb
@@ -1,39 +1,42 @@
-require "graphql/client"
-require "graphql/client/http"
+# frozen_string_literal: true
+
+require 'graphql/client'
+require 'graphql/client/http'
 class GithubInfoController < ApplicationController
-    def index
-    end
+  before_action :get_github_data, only: [:show]
+  def index; end
 
-    def show
-        @query_result = CLIENT.query(GITHUB_QUERY, variables: {username: params[:gh_login]}).to_h
-        if @query_result["data"]["user"]
-            @github_login = @query_result["data"]["user"]["login"]
-            @github_name = @query_result["data"]["user"]["name"]
-            @github_repos = @query_result["data"]["user"]["repositories"]["nodes"]
-        else
-            redirect_to root_path, notice: "Oops! Something went wrong!"
-        end
-    end
+  def show
+    @github_login = @query_result['data']['user']['login']
+    @github_name = @query_result['data']['user']['name']
+    @github_repos = @query_result['data']['user']['repositories']['nodes']
+  end
 
-    private
-    GITHUB_ACCESS_TOKEN = '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'
-    HTTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
-        def headers(context)
-          { 
-            "Authorization" => "Bearer #{GITHUB_ACCESS_TOKEN}",
-          }
-        end
-    end 
-    
-    CLIENT = GraphQL::Client.new(schema: GraphQL::Client.load_schema("db/schema.json"), 
-                                 execute: HTTP)
-    
-    GITHUB_QUERY = CLIENT.parse <<-'GRAPHQL'
+  private
+
+  def get_github_data
+    @query_result = CLIENT.query(GITHUB_QUERY, variables: { username: params[:gh_login] }).to_h
+    redirect_to root_path, notice: @query_result['errors'][0]['message'] if @query_result.key?('errors')
+  end
+
+  GITHUB_ACCESS_TOKEN = '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'
+  HTTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+    def headers(_context)
+      {
+        'Authorization' => "Bearer #{GITHUB_ACCESS_TOKEN}"
+      }
+    end
+  end
+
+  CLIENT = GraphQL::Client.new(schema: GraphQL::Client.load_schema('db/schema.json'),
+                               execute: HTTP)
+
+  GITHUB_QUERY = CLIENT.parse <<-'GRAPHQL'
     query($username: String!) {
         user(login: $username) {
             login
             name
-            repositories(first: 50, isFork: false) {
+            repositories(first: 50) {
                 nodes {
                     name
                     url
@@ -41,5 +44,5 @@ class GithubInfoController < ApplicationController
             }
         }
     } 
-    GRAPHQL
+  GRAPHQL
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "github_info#index"
-  get "show", to: "github_info#show"
+  root 'github_info#index'
+  get 'show', to: 'github_info#show'
 end
